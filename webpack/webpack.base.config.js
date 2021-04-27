0aed180be47ded4edfa796b8a6fa149c8b0b4ac8
Nick_J <925416729@qq.com>
2021-04-27 15:56:25 +0800

    1. add new plugin to handle react class

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -53,7 +53,8 @@ module.exports = {
                         presets: [
                             ['@babel/preset-env', {useBuiltIns: 'usage', 'corejs': '2'}],
                             '@babel/preset-react'
-                        ]
+                        ],
+                        plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 }
             }
